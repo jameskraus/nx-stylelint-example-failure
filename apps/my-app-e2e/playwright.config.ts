75ac628eb0639f7b81e75b7eb366ba113a6c8408
James Kraus <jamespkraus@gmail.com>
2025-11-22 12:59:26 -0500

    Initial commit

--- apps/my-app-e2e/playwright.config.ts
+++ apps/my-app-e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
 
 /**
  * Read environment variables from file.
@@ -24,9 +24,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command:
-      'npx nx run @nx-stylint-example-failure/nx-stylint-example-failure:dev',
-    url: 'http://localhost:3000',
+    command: 'npx nx run @nx-stylint-example-failure/my-app:preview',
+    url: 'http://localhost:4200',
     reuseExistingServer: true,
     cwd: workspaceRoot,
   },
---
+++ stylelint.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import("stylelint").Config} */
+export default {
+  rules: {},
+  customSyntax: "postcss-styled-syntax"
+};
